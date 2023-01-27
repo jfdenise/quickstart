abca13ef5a98b74a786e239170a5939823bb5bbc
JF Denise <jdenise@redhat.com>
2023-01-27 10:41:00 +0100

    Foo

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "FOUFOU Hello " + name + "!";
+        return "BABA Hello " + name + "!";
     }
 
 }
